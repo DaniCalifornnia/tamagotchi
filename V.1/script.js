c5eeaa2881d77f58563db627d9258625038e5c49
DaniCalifornnia <danirincon8@gmail.com>
2023-08-01 22:16:39 -0500

    js working

--- V.1/script.js
+++ V.1/script.js
@@ -1,72 +1,96 @@
+
 var life = 100;
 var clean = 100;
 var sleep = 100;
 var fun = 100;
 var social = 100;
 
-function loop(time=2){
-    var total = life + clean + sleep + fun + social;
 
-    if (life <= 0 || clean <= 0 || sleep <= 0 || fun <= 0 || social <= 0){
-        document.getElementById('pet').src="/Art/Cat1.png"
-        document.getElementById('pet').innerHTML = 'Meow Died, pls restart';
-    }else if (total > 400){
-        document.getElementById('pet').src"/Art/Cat0.png";
-    }else if (total > 300){
-        document.getElementById('pet').src"/Art/Cat0.png";
-    }else if (total > 200){
-        document.getElementById('pet').src"/Art/Cat2.png";
-    }else if (total > 100){
-        document.getElementById('pet').src"/Art/Cat3.png";
-    }else if (total > 50){
-        document.getElementById('pet').src"/Art/Cat1.png";
-    }
+function Loop(time = 2){
 
-    life = life - parseInt(time);
-    clean = clean - parseInt(time);
-    sleep = sleep - parseInt(time);
-    fun = fun - parseInt(time);
-    social = social - parseInt(time);
+  var total = life + clean + sleep + fun + social;
 
-    document.getElementById('life').innerHTML = life + '%';
-    document.getElementById('clean').innerHTML = clean + '%';
-    document.getElementById('sleep').innerHTML = sleep + '%';
-    document.getElementById('fun').innerHTML = fun + '%';
-    document.getElementById('social').innerHTML = social + '%';
+
+  if(life <= 0 || clean <= 0 || sleep <= 0 || fun <= 0 || social <= 0){
+     document.getElementById("pet").src="/Art/Cat1.png";
+     document.getElementById('message').innerHTML = 'Meow Died, pls restart';
+  }
+  else if(total > 400){
+     document.getElementById("pet").src="/Art/Cat0.png";
+  }
+  else if(total > 300){
+     document.getElementById("pet").src="/Art/Cat0.png";
+  }
+  else if(total > 200){
+     document.getElementById("pet").src="/Art/Cat2.png";
+  }
+  else if(total > 100){
+     document.getElementById("pet").src="/Art/Cat3.png";
+  }
+  else if(total > 50){
+     document.getElementById("pet").src="/Art/Cat1.png";
+  }
+
+  life = life - parseInt(time);
+  clean = clean - parseInt(time);
+  sleep = sleep - parseInt(time);
+  fun = fun - parseInt(time);
+  social = social - parseInt(time);
+
+
+  document.getElementById('life').style.width = life + 'px';
+  document.getElementById('clean').style.width = clean + 'px';
+  document.getElementById('sleep').style.width = sleep + 'px';
+  document.getElementById('fun').style.width = fun + 'px';
+  document.getElementById('social').style.width = social + 'px';
+
+  
+  document.getElementById('life').innerHTML = life + '%';
+  document.getElementById('clean').innerHTML = clean + '%';
+  document.getElementById('sleep').innerHTML = sleep + '%';
+  document.getElementById('fun').innerHTML = fun + '%';
+  document.getElementById('social').innerHTML = social + '%';
 }
 
-function Start (){
-    var temporizador = setInterval(loop, 1000);
+
+function Start(){
+  var temporizador = setInterval(Loop, 1000);
 }
+
 
 function Eat(){
-    life = 100
-    document.getElementById('life').innerHTML = life + '%';
-    document.getElementById('life').innerHTML = life + 'px';
+  fome = 100;
+  document.getElementById('fome').innerHTML = fome + '%';
+  document.getElementById('fome').style.width = fome + 'px';
 }
 
-function Showert(){
-    clean = 100
-    document.getElementById('clean').innerHTML = clean + '%';
-    document.getElementById('clean').innerHTML = clean + 'px';
+
+function Shower(){
+  higiene = 100;
+  document.getElementById('higiene').innerHTML = higiene + '%';
+  document.getElementById('higiene').style.width = higiene + 'px';
 }
+
 
 function Sleep(){
-    sleep = 100
-    document.getElementById('sleep').innerHTML = sleep + '%';
-    document.getElementById('sleep').innerHTML = sleep + 'px';
+  sono = 100;
+  document.getElementById('sono').innerHTML = sono + '%';
+  document.getElementById('sono').style.width = sono + 'px';
 }
+
 
 function Play(){
-    fun = 100
-    document.getElementById('fun').innerHTML = fun + '%';
-    document.getElementById('fun').innerHTML = fun + 'px';
+  diversao = 100;
+  document.getElementById('diversao').innerHTML = diversao + '%';
+  document.getElementById('diversao').style.width = diversao + 'px';
 }
 
+
 function Meow(){
-    social = 100
-    document.getElementById('social').innerHTML = social + '%';
-    document.getElementById('social').innerHTML = social + 'px';
+  social = 100;
+  document.getElementById('social').innerHTML = social + '%';
+  document.getElementById('social').style.width = social + 'px';
 }
+
 
 Start();
